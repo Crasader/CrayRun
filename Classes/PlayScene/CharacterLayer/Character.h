3a20a335237e3919aa9d28d0e1901f0b248999bb
Yuga Yamamoto <srise1103@gmail.com>
2017-05-15 15:39:21 +0900

    スライムクラス（キャラクタ）の作成（中身空）、ゲームオーバーが左側にも判定するように変更

--- Classes/PlayScene/CharacterLayer/Character.h
+++ Classes/PlayScene/CharacterLayer/Character.h
@@ -56,5 +56,7 @@ public:
 	}
 	int JumpCnt;//ジャンプフラグ
 
+	float JumpSize;//ジャンプするおおきさ
+
 };
 
---
+++ Classes/PlayScene/CharacterLayer/Slime.h
@@ -0,0 +1,21 @@
+/// <summary>
+/// スライムキャラクター
+/// GS2 24 山本悠雅
+/// 2017/05/15
+/// </summary>
+
+#pragma once
+/* ---- ライブラリのインクルード ---------- */
+#include "cocos2d.h"
+#include "Character.h"
+
+class Slime : public Character
+{
+public:
+	bool init();
+	CREATE_FUNC(Slime);
+
+	void Jump();//ジャンプする
+
+private:
+};
