cc590becda3e9b95976dfe22f9c1c09007137d1e
Yuga Yamamoto <srise1103@gmail.com>
2017-05-25 15:56:45 +0900

    タッチ終了後とタッチキャンセル時の処理を削除

--- Classes/ResultScene/ResultLayer.h
+++ Classes/ResultScene/ResultLayer.h
@@ -28,10 +28,6 @@ private:
 	void onTouchesBegan(const std::vector<cocos2d::Touch*>& touches, cocos2d::Event *unused_event);
 	//スワイプ時に呼び出される
 	void onTouchesMoved(const std::vector<cocos2d::Touch*>& touches, cocos2d::Event *unused_event);
-	//タッチが離された
-	void onTouchesEnded(const std::vector<cocos2d::Touch*>& touches, cocos2d::Event *unused_event);
-	//タッチ異常終了
-	void onTouchesCancelled(const std::vector<cocos2d::Touch*>&touches, cocos2d::Event *unused_event);
 	//リザルトスコア
 	ResultScore* m_resultscore;
 	//背景画像
